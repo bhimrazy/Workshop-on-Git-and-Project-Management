f50ab770c78388a27647f5f15e817927be56beb9
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-12-30 10:50:24 +0000

    Added a example for github action

---
+++ example_for_github_actions_in_python/main.py
@@ -0,0 +1,10 @@
+def find_sum(a: int, b: int) -> int:
+    """ This function returns the sum of two number
+
+    Args:
+        a (int): first argument
+        b (int): second argument
+    Returns:
+        int: Result
+    """
+    return a+b
